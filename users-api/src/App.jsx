19858bcb80b2c8e4839088abe742c35818836e18
jfernandezcl <javierfernandez0987@gmail.com>
2024-05-20 11:46:06 +0200

    Title

--- users-api/src/App.jsx
+++ users-api/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 
 import { ListOfUsers } from "./components/ListOFUsers/ListOfUsers";
+import { Title } from "./components/Title/Title";
 import { useDataAPI } from "./components/hooks/useDataAPI";
 
 export function App() {
@@ -9,7 +10,7 @@ export function App() {
   return (
     <main>
       <div>
-        <h1>API user data</h1>
+        <Title title={"API user data"} />
       </div>
       <section>
         <div>
