aaf4413bd9e5267718ef0baefc0891c0cb80d3a4
jfernandezcl <javierfernandez0987@gmail.com>
2024-05-21 15:47:00 +0200

    Botton

--- users-api/src/App.jsx
+++ users-api/src/App.jsx
@@ -6,7 +6,7 @@ import { Title } from "./components/Title/Title";
 import { useDataAPI } from "./components/hooks/useDataAPI";
 
 export function App() {
-  const { data, loading, error } = useDataAPI(
+  const { data, loading, error, handleCancelRequest } = useDataAPI(
     "https://jsonplaceholder.typicode.com/users"
   );
 
@@ -14,6 +14,9 @@ export function App() {
     <main>
       <div className="title-card">
         <Title title={"API user data"} />
+      </div>
+      <div className="botton-cancel">
+        <button onClick={handleCancelRequest}>Cancel request</button>
       </div>
       <section>
         <div className="users-card">
--- users-api/src/components/hooks/useDataAPI.js
+++ users-api/src/components/hooks/useDataAPI.js
@@ -3,16 +3,36 @@ import { useState } from "react";
 
 export function useDataAPI(url) {
   const [data, setData] = useState();
+  const [controller, setController] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortController = new AbortController();
+    setController(abortController);
     setLoading(true);
-    fetch(url)
+
+    fetch(url, { signal: abortController.signal })
       .then((response) => response.json())
       .then((data) => setData(data))
-      .catch((error) => setError(error))
+      .catch((error) => {
+        if (error.name === "AbortError") {
+          console.log("Request cancelled");
+        } else {
+          setError(error);
+        }
+      })
       .finally(() => setLoading(false));
+
+    return () => abortController.abort();
   }, []);
+
+  const handleCancelRequest = () => {
+    if (controller) {
+      controller.abort();
+      setError("Request cancelled");
+    }
+  };
+
   return { data, loading, error };
 }
