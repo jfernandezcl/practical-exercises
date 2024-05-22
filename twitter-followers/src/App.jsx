af9ef77e7735ab01fc908ebca103b6825b151ef0
jfernandezcl <javierfernandez0987@gmail.com>
2024-05-22 16:30:52 +0200

    create button functionality and styles

--- twitter-followers/src/App.jsx
+++ twitter-followers/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { FollowCard } from "./components/FollowCard";
 
 export function App() {
+  const midudev = { isFollowing: true, userName: "midudev" };
+  const hernan = { isFollowing: false, userName: "hernan" };
   return (
     <section className="App">
-      <FollowCard isFollowing userName="midudev">
+      <FollowCard {...midudev}>
         <strong>Miguel Ángel Durán</strong>
       </FollowCard>
-      <FollowCard isFollowing={false} userName="hernan">
+      <FollowCard {...hernan}>
         <strong>Pablo Hernandez</strong>
       </FollowCard>
     </section>
--- twitter-followers/src/components/FollowCard.jsx
+++ twitter-followers/src/components/FollowCard.jsx
@@ -1,5 +1,17 @@
-export function FollowCard({ children, userName, isFollowing }) {
-  console.log(isFollowing);
+import { useState } from "react";
+
+export function FollowCard({ children, userName }) {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = isFollowing
+    ? "followCard-button is-following"
+    : "followCard-button";
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
+
   return (
     <article className="followCard">
       <header className="followCard-header">
@@ -9,13 +21,15 @@ export function FollowCard({ children, userName, isFollowing }) {
           src={`https://unavatar.io/${userName}`}
         ></img>
         <div className="followCard-info">
-          {children}
+          <strong>{children}</strong>
           <span className="followCard-infoUser">@{userName}</span>
         </div>
       </header>
 
       <aside>
-        <button className="followCard-button">Seguir</button>
+        <button className={buttonClassName} onClick={handleClick}>
+          {text}
+        </button>
       </aside>
     </article>
   );
