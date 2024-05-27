d0057d0c03f5f0d13bad0baf2f88b2744003eef5
jfernandezcl <javierfernandez0987@gmail.com>
2024-05-27 13:44:50 +0200

    created new button

--- mouse-follower/src/App.jsx
+++ mouse-follower/src/App.jsx
@@ -1,18 +1,47 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [enable, setEnable] = useState(false);
+  const [enabled, setEnable] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    console.log("efecto");
-  });
+    console.log("effect", { enabled });
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      console.log("handleMove", { clientX, clientY });
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+
+    return () => {
+      console.log("cleanup");
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
   return (
-    <>
-      <h3>Proyecto 3</h3>
-      <button onClick={() => setEnable(!enable)}>
-        {enable ? "Desactivar" : "Activar"} seguir puntero
+    <main>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#09f",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <button onClick={() => setEnable(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
       </button>
-    </>
+    </main>
   );
 }
 
