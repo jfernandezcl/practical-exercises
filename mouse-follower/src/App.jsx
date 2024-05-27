d3e0c96ecf7624ec950d845a556c7c42e1731dc4
jfernandezcl <javierfernandez0987@gmail.com>
2024-05-27 12:12:37 +0200

    created the button and functionality

--- mouse-follower/src/App.jsx
+++ mouse-follower/src/App.jsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <h3>Proyecto 3</h3>;
+  const [enable, setEnable] = useState(false);
+
+  useEffect(() => {
+    console.log("efecto");
+  });
+  return (
+    <>
+      <h3>Proyecto 3</h3>
+      <button onClick={() => setEnable(!enable)}>
+        {enable ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </>
+  );
 }
 
 export default App;
