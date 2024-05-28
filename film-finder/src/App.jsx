918f0d28c72dd45447f5d71c0b9c7dd34fb7ee0d
jfernandezcl <javierfernandez0987@gmail.com>
2024-05-28 13:28:14 +0200

    created components

--- film-finder/src/App.jsx
+++ film-finder/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import responseMovies from "./mocks/with-results.json";
 import withouResults from "./mocks/no-results.json";
+import { Movies } from "./components/Movies.jsx";
 
 function App() {
   const movies = responseMovies.Search;
-  const hasMovies = movies?.length > 0;
+
   return (
     <div className="page">
       <header>
@@ -16,19 +17,7 @@ function App() {
       </header>
 
       <main>
-        {hasMovies ? (
-          <ul>
-            {movies.map((movie) => (
-              <li key={movie.imdbID}>
-                <h3>{movie.Title}</h3>
-                <p>{movie.Year}</p>
-                <img src={movie.Poster} alt={movie.Title} />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No se encontraron películas para esta búsqueda</p>
-        )}
+        <Movies movies={movies} />
       </main>
     </div>
   );
