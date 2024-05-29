b408e2b43372605e57e8dd7dc3daffafc688f763
jfernandezcl <javierfernandez0987@gmail.com>
2024-05-29 11:18:11 +0200

    updated search and button

--- film-finder/src/App.jsx
+++ film-finder/src/App.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies.jsx";
 import { useMovies } from "./hooks/useMovies.js";
 
 function App() {
   const { movies } = useMovies();
+  const [query, setQuery] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const { query } = Object.fromEntries(new window.FormData(event.target));
     console.log(query);
+  };
+
+  const handlechange = (event) => {
+    setQuery(event.target.value);
   };
 
   return (
@@ -17,6 +23,8 @@ function App() {
         <h1>Buscador de películas</h1>
         <form className="form" onSubmit={handleSubmit}>
           <input
+            onChange={handlechange}
+            value={query}
             name="query"
             placeholder="Avengers, Star Wars, The Matrix..."
           />
