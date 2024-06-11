620b9de470d096ee8b096798ac1cf7e847f21a02
jfernandezcl <javierfernandez0987@gmail.com>
2024-06-11 16:51:53 +0200

    created add and delete

--- simple-notes-application/README.md
+++ simple-notes-application/README.md
@@ -1,8 +1,6 @@
-# React + Vite
+Título:
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Aplicación de Notas Simples en React
+Descripción:
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Crear una aplicación de notas simples utilizando React. La aplicación permitirá a los usuarios escribir nuevas notas, ver una lista de notas existentes y eliminar notas según sea necesario. La interfaz de usuario constará de un campo de entrada para escribir nuevas notas, un botón para agregar una nota, y una lista de notas con botones de eliminación asociados.
--- simple-notes-application/src/App.jsx
+++ simple-notes-application/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Estado para almacenar las notas
+  const [notas, setNotas] = useState([]);
+  // Estado para almacenar la nueva nota
+  const [nuevaNota, setNuevaNota] = useState("");
+
+  // Función para agregar una nueva nota
+  const agreagarNota = () => {
+    if (nuevaNota.trim() !== "") {
+      setNotas([...notas, nuevaNota]);
+      setNuevaNota("");
+    }
+  };
+
+  // Función para eliminar una nota
+  const eliminarNota = (indice) => {
+    const nuevasNotas = notas.filter((notas, i) => i !== indice);
+    setNotas(nuevasNotas);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Simple notas Application</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
