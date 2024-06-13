dc5bbef42aa738846ccbd5747a410aac7ab6e9ad
jfernandezcl <javierfernandez0987@gmail.com>
2024-06-13 12:48:20 +0200

    created components

---
+++ web-barberia/src/components/AboutMe.jsx
@@ -0,0 +1,5 @@
+const AboutMe = () => {
+  return <h1>Sobre mi</h1>;
+};
+
+export default AboutMe;
---
+++ web-barberia/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Página Personal - Barbería</h1>
+    </header>
+  );
+};
+
+export default Header;
