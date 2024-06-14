3fdb473feff6c6a708e09e764eaeb672764f01a1
jfernandezcl <javierfernandez0987@gmail.com>
2024-06-14 11:58:03 +0200

    created styles header

--- web-barberia/src/components/Header.jsx
+++ web-barberia/src/components/Header.jsx
@@ -1,10 +1,26 @@
-// @ts-nocheck
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
     <header>
       <div></div>
+      <div className="h-container-list">
+        <ul>
+          <li>
+            <a href="#inicio">INICIO</a>
+          </li>
+          <li>
+            <a href="#perfil">PERFIL</a>
+          </li>
+          <li>
+            <a href="#productos">PRODUCTOS</a>
+          </li>
+          <li>
+            <a href="#contacto">CONTACTO</a>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
