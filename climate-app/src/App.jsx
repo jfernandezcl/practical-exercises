886d512a5ff4b32dadea55e84cbd4efe8e6ea864
jfernandezcl <javierfernandez0987@gmail.com>
2024-06-20 16:22:44 +0200

    modified app

--- climate-app/src/App.jsx
+++ climate-app/src/App.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${
+    // @ts-ignore
+    import.meta.env.VITE_API_KEY
+  }&q=London&aqi=no`;
+  const [searchData, setSearchData] = useState();
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => setSearchData(data));
+  }, [setSearchData]);
+
   return (
-    <>
-      <h1>Climate App</h1>
-    </>
+    <main>
+      <div>
+        <h1>Buscador de clima:</h1>
+      </div>
+
+      <form>
+        <label>
+          Ciudad: <input type="text" />
+        </label>
+        <button type="submit">Buscar</button>
+      </form>
+    </main>
   );
 }
 
