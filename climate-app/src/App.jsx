264076a55981b732d03798babee60c603e53c68e
jfernandezcl <javierfernandez0987@gmail.com>
2024-06-24 13:19:42 +0200

    created compoenents

--- climate-app/src/App.jsx
+++ climate-app/src/App.jsx
@@ -3,16 +3,30 @@ import "./App.css";
 
 function App() {
   const [searchData, setSearchData] = useState(null);
-  const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${
-    // @ts-ignore
-    import.meta.env.VITE_API_KEY
-  }&q=London&aqi=no`;
+  const [city, setCity] = useState("");
+  const [inputCity, setInputCity] = useState("");
 
   useEffect(() => {
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => setSearchData(data));
-  }, [apiUrl]);
+    if (city) {
+      const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${
+        // @ts-ignore
+        import.meta.env.VITE_API_KEY
+      }&q={city}&aqi=no`;
+
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => setSearchData(data));
+    }
+  }, [city]);
+
+  const handleInputChange = (event) => {
+    setInputCity(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCity(inputCity);
+  };
 
   return (
     <main>
@@ -20,12 +34,26 @@ function App() {
         <h1>Buscador de clima:</h1>
       </div>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
-          Ciudad: <input type="text" />
+          Ciudad:
+          <input
+            className="form-input"
+            type="text"
+            value={inputCity}
+            onChange={handleInputChange}
+          />
         </label>
         <button type="submit">Buscar</button>
       </form>
+
+      {searchData && (
+        <div>
+          <h2>Clima en {searchData.location.name}:</h2>
+          <p>Temperatura: {searchData.current.temp_c}°C</p>
+          <p>Condición: {searchData.current.condition.text}</p>
+        </div>
+      )}
     </main>
   );
 }
