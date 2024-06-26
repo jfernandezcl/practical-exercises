be62419f632d86a4e1ac470d39143c70acebc0d8
jfernandezcl <javierfernandez0987@gmail.com>
2024-06-26 11:45:58 +0200

    created images and icons

--- climate-app/src/App.jsx
+++ climate-app/src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import Formulario from "./components/Formulario";
+// @ts-ignore
+import localizacion from "./images/localizacion.png";
 
 const App = () => {
   return (
     <main>
+      <div className="titulo-icono">
+        <h1>Buscador de clima</h1>
+        <img src={localizacion} alt="icono" />
+      </div>
+
       <div>
-        <h1 className="titulo">Buscador de clima</h1>
         <Formulario />
       </div>
     </main>
