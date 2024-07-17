a5686cf30d88da2761af93227339a11dd7519fa2
jfernandezcl <javierfernandez0987@gmail.com>
2024-07-17 11:12:10 +0200

    created three constants

--- todo-app-ts/src/App.tsx
+++ todo-app-ts/src/App.tsx
@@ -1,12 +1,28 @@
-
 import './App.css'
 
-function App() {
+const mockTodos = [
+  {
+    id: '1',
+    title: 'todo 1' ,
+    completed: false,
+  },
+  {
+    id: '2',
+    title: 'todo 2' ,
+    completed: false,
+  },
+  {
+    id: '3',
+    title: 'todo 3' ,
+    completed: false,
+  }
+]
+
+
+const App: React.FC = () => {
 
   return (
-    <>
       <h1>Todo App ts</h1>
-    </>
   )
 }
 
