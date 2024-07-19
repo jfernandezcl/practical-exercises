2bbf79a68b673090ca3feac1eb13a48cc8746004
jfernandezcl <javierfernandez0987@gmail.com>
2024-07-19 11:21:46 +0200

    modified completed

--- todo-app-ts/src/App.tsx
+++ todo-app-ts/src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Todos } from './components/Todos'
 import './App.css'
-import { TodoId, ListOfTodos } from './components/types';
+import { TodoId, ListOfTodos, type Todo as Todo } from './components/types';
+
 
 const mockTodos: ListOfTodos = [
   {
@@ -30,6 +31,8 @@ const App = (): JSX.Element => {
   setTodos(newTodos)
   }
 
+  const handleCompleted = (
+    { id, completed } : Pick<TodoType, 'id' | 'completed'>): void => {}
 
   return (
       <div className='todoapp'>
