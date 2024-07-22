7b9051f4a2b6e2c4739a879b01856010d994d57e
jfernandezcl <javierfernandez0987@gmail.com>
2024-07-22 14:03:48 +0200

    created test exercise 1

---
+++ prueba-tecnica-junior-01/test/index.test.js
@@ -0,0 +1,23 @@
+import {
+  ping,
+  obtenerDatosPromise,
+  procesarArchivoPromise,
+  procesarArchivo,
+  leerArchivos,
+} from "../solutions/index.js";
+
+import { describe, it, beforeEach, afterEach } from "node:test";
+import { equal, ifError } from "node:assert/strict";
+import { unlinkSync, writeFileSync } from "node:fs";
+import { readFile } from "node:fs/promises";
+import { createRequire } from "node:module";
+
+describe("1. ping", () => {
+  it("1.1. ping midu.dev", (_, done) => {
+    ping("midu.dev", (err, info) => {
+      ifError(err);
+      equal(info.ip, "midu.dev");
+      done();
+    });
+  });
+});
