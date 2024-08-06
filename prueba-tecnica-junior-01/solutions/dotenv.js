1ecb6704d0d88f5cc39e4fbcfb838aad163a4cb8
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-06 10:48:59 +0200

    separate string

--- prueba-tecnica-junior-01/solutions/dotenv.js
+++ prueba-tecnica-junior-01/solutions/dotenv.js
@@ -4,6 +4,7 @@ import fs from "node:fs/promises";
 export function config({ path = ".env" } = {}) {
   try {
     const env = fs.readFile(path, "utf8");
+    const line = env.split("\n");
   } catch (error) {
     console.error(e);
   }
