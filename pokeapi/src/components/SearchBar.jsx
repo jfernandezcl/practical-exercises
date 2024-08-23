bb2e82ed04c7b33015f242e1068db12c69e98c30
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-23 20:23:24 +0200

    created input

--- pokeapi/src/components/SearchBar.jsx
+++ pokeapi/src/components/SearchBar.jsx
@@ -2,4 +2,20 @@ import { useState } from "react";
 
 export function SearchBar({ onSearch }) {
   const [searchTerm, setSearchTerm] = useState('')
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+    setSearchTerm('')
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Buscar pokémon..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+    </div>
+  )
 }
