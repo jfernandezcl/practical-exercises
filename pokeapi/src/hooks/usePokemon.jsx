ee258e8df08aa95ea8848833438951956a5d406e
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-23 13:49:58 +0200

    finished to make the request

--- pokeapi/src/hooks/usePokemon.jsx
+++ pokeapi/src/hooks/usePokemon.jsx
@@ -4,6 +4,14 @@ export const usePokemon = () => {
   const [pokemons, setPokemons] = useState([]);
   const [filteredPokemons, setFilteredPokemons] = useState([]);
 
-  useEffect(() => { }, []);
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const responses = await Promise.all(
+        Array.from({ length: 150 }, (_, i) =>
+          fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`)
+        )
+      );
+    }
+  }, []);
 
 }
