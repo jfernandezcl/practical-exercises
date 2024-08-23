25562f192849a890a2cfb46341704e4034a776ac
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-23 13:04:53 +0200

    created the state and filtering and also imported the useState

---
+++ pokeapi/src/hooks/usePokemon.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react"
+
+export const usePokemon = () => {
+  const [pokemons, setPokemons] = useState([])
+  const [filteredPokemons, setFilteredPokemons] = useState([])
+
+}
