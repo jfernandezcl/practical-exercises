146b8a74502b8c7ae2c2dcc6114b185b24ba4e7d
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-24 17:28:19 +0200

    new component created for the header with file for styles

--- pokeapi/src/App.jsx
+++ pokeapi/src/App.jsx
@@ -1,26 +1,13 @@
 import './css/app.css'
-import logo from './images/logo.png'
+import Header from './components/Header'
 import PokemonList from './components/PokemonList'
 
 function App() {
 
   return (
     <>
-      <div className='container-banner'>
-        <ul>
-          <li>
-            <a href='#home'>Home</a>
-          </li>
-          <li>
-            <a href='#search'>Search</a>
-          </li>
-          <li>
-            <a href='#about'>About</a>
-          </li>
-        </ul>
-      </div>
       <div>
-        <img className='logo' src={logo} alt='logo' />
+        <Header />
       </div>
       <div>
         <PokemonList />
---
+++ pokeapi/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import logo from './images/logo.png'
+import '../css/Header.css'
+
+export default function Header() {
+  return (
+    <header>
+      <div className='container-banner'>
+        <ul>
+          <li>
+            <a href='#home'>Home</a>
+          </li>
+          <li>
+            <a href='#search'>Search</a>
+          </li>
+          <li>
+            <a href='#about'>About</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <img className='logo' src={logo} alt='logo' />
+      </div>
+    </header>
+  )
+}
