3870bef55d39cb65b82aea545ac6e06361d3e18c
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-24 17:20:44 +0200

    created button list

--- pokeapi/src/App.jsx
+++ pokeapi/src/App.jsx
@@ -7,7 +7,17 @@ function App() {
   return (
     <>
       <div className='container-banner'>
-        <h1>Poké API</h1>
+        <ul>
+          <li>
+            <a href='#home'>Home</a>
+          </li>
+          <li>
+            <a href='#search'>Search</a>
+          </li>
+          <li>
+            <a href='#about'>About</a>
+          </li>
+        </ul>
       </div>
       <div>
         <img className='logo' src={logo} alt='logo' />
