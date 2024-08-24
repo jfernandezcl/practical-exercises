751eb830fceabb9c20defbf05dc6887fa0d4cbe6
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-24 18:14:57 +0200

    changes to the logo images for the masthead

--- pokeapi/src/components/Header.jsx
+++ pokeapi/src/components/Header.jsx
@@ -3,23 +3,21 @@ import '../css/Header.css'
 
 export default function Header() {
   return (
-    <header>
-      <div className='container-banner'>
-        <ul>
-          <li>
-            <a href='#home'>Home</a>
-          </li>
-          <li>
-            <a href='#search'>Search</a>
-          </li>
-          <li>
-            <a href='#about'>About</a>
-          </li>
-        </ul>
-      </div>
+    <header className='container-banner'>
       <div>
-        <img className='logo' src={logo} alt='logo' />
+        <img src={logo} alt='logo' />
       </div>
-    </header>
+      <ul>
+        <li>
+          <a href='#home'>Home</a>
+        </li>
+        <li>
+          <a href='#search'>Search</a>
+        </li>
+        <li>
+          <a href='#about'>About</a>
+        </li>
+      </ul>
+    </header >
   )
 }
