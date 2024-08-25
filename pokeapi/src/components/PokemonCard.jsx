28b6b8ae49960a1de4632cd45b121e68aca0de6d
jfernandezcl <javierfernandez0987@gmail.com>
2024-08-25 21:23:42 +0200

    new styles card

--- pokeapi/src/components/PokemonCard.jsx
+++ pokeapi/src/components/PokemonCard.jsx
@@ -4,9 +4,9 @@ export default function PokemonCard({ pokemon }) {
   return (
     <div className="container-card">
       <div className='card-pokemon'>
-        <img src={pokemon.image} alt={pokemon.name} />
+        <img className='card-imagen' src={pokemon.image} alt={pokemon.name} />
       </div>
-      <p>{pokemon.name}</p>
+      <p className='card-name'>{pokemon.name}</p>
     </div>
   )
 }
